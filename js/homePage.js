9f07397def976802b43ff2d085f6df14126a2bd9
PadriPudru <daniel.pa.wk@gmail.com>
2025-03-17 17:13:50 +0000

    Added responsive implementation for home page.

---
+++ js/homePage.js
@@ -0,0 +1,7 @@
+if (document.body.id === 'homePage') {
+    loadTemplate('../main/homeMain.html', 'main');
+
+    loadTemplate('../templates/cardMenu.html', 'item');
+    loadTemplate('../templates/cardMenu.html', 'item2');
+    loadTemplate('../templates/cardMenu.html', 'item3');
+}
