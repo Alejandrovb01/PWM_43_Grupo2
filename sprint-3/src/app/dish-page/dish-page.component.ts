d17685e51bac711bed803ad8114e701e934dd70d
GabrielDT13 <gabrieldomingueztorres@gmail.com>
2025-04-04 15:42:01 +0100

    wip: migrate to angular

---
+++ sprint-3/src/app/dish-page/dish-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-page',
+  imports: [],
+  templateUrl: './dish-page.component.html',
+  styleUrl: './dish-page.component.css'
+})
+export class DishPageComponent {
+
+}
