13e4fd8f1446a246f59faea46019a7065bceaf5b
PadriPudru <daniel.pa.wk@gmail.com>
2025-04-28 17:31:05 +0100

    login for workers

--- sprint-3/src/app/login/login.component.ts
+++ sprint-3/src/app/login/login.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service'; // Ajusta el path según tu proyecto
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports: [
+    FormsModule,
+  ]
+
 })
 export class LoginComponent {
+  usuarioInput: string = '';
+  passwordInput: string = '';
 
+  constructor(private apiService: ApiService, private router: Router) {}
+
+  login() {
+    this.apiService.getUsuarios().subscribe((usuarios: any[]) => {
+      const usuarioEncontrado = usuarios.find(data =>
+        data['Usuario'] === this.usuarioInput && data['Contraseña'] === this.passwordInput
+      );
+
+      if (usuarioEncontrado) {
+        console.log('Login exitoso');
+        this.router.navigate(['/kitchen']);
+      } else {
+        console.error('Usuario o contraseña incorrectos');
+        alert('Usuario o contraseña incorrectos');
+      }
+    }, error => {
+      console.error('Error al obtener los usuarios', error);
+    });
+  }
 }
