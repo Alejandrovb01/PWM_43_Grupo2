18a25dbcf364894095f4d050acb01edda7140587
alejandro <alejandrovb01@gmail.com>
2025-05-01 15:04:17 +0100

    Footer y Header son ahora standalone.

--- sprint-3/src/app/footer/footer.component.ts
+++ sprint-3/src/app/footer/footer.component.ts
@@ -5,6 +5,7 @@ import {RouterLink} from '@angular/router';
   selector: 'app-footer',
   imports: [RouterLink],
   templateUrl: './footer.component.html',
+  standalone: true,
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
