0b12cf5023fb0738e6b8f57732e11ba02e5939d5
GabrielDT13 <gabrieldomingueztorres@gmail.com>
2025-05-08 13:24:51 +0100

    init project

--- sprint-4/src/app/app.routes.ts
+++ sprint-4/src/app/app.routes.ts
@@ -1,13 +1,26 @@
-import { Routes } from '@angular/router';
+import {Routes} from "@angular/router";
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
+  /*{
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent),
+  },*/
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    loadComponent: () => import('./layout/layout.component').then(m => m.LayoutComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./menu/menu.component').then(m => m.MenuComponent)
+      },
+      {
+        path: 'dish/:id',
+        loadComponent: () => import('./dish-page/dish-page.component').then(m => m.DishPageComponent)
+      }
+    ]
   },
+  {
+    path: '**',
+    redirectTo: ''
+  }
 ];
