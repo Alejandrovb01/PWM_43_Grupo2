f118991e3dfcaaa80f10bb6bcb664042a83d5eb5
GabrielDT13 <gabrieldomingueztorres@gmail.com>
2025-05-09 11:37:23 +0100

    update project

--- sprint-4/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ulpgc.luigis',
-  appName: 'sprint-4',
-  webDir: 'www'
-};
-
-export default config;
