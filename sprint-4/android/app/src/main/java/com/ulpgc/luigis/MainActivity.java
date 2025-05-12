f0490ae3fc3966dd761354864a68f6995f0bf340
GabrielDT13 <gabrieldomingueztorres@gmail.com>
2025-05-12 20:43:45 +0100

    update project

---
+++ sprint-4/android/app/src/main/java/com/ulpgc/luigis/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ulpgc.luigis;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- sprint-4/src/app/menu/menu.component.ts
+++ sprint-4/src/app/menu/menu.component.ts
@@ -1,8 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FirebaseDataService } from '../services/firebase-data.service';
-import { NgForOf, NgIf, AsyncPipe } from '@angular/common';
+import { NgIf, NgForOf } from '@angular/common';
 import { CardSliderComponent } from '../card-slider/card-slider.component';
-import { Observable, map, tap } from 'rxjs';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -12,24 +11,32 @@ import { IonicModule } from '@ionic/angular';
     NgIf,
     NgForOf,
     CardSliderComponent,
-    AsyncPipe,
     IonicModule,
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {
-  menuItems$: Observable<{ [category: string]: any[] }> = this.firebaseDataService.getData('menu').pipe(
-    tap(() => this.loading = true),
-    map(items => items.filter((item: any) => item.stock)),
-    map(items => this.groupByCategory(items)),
-    tap(() => this.loading = false)
-  );
-
+export class MenuComponent implements OnInit {
+  menuItems: { [category: string]: any[] } = {};
   categoryOrder: string[] = ['Clásicas', 'Bestsellers', 'Gourmet', 'Appetizers', 'Antipasti'];
   loading: boolean = true;
 
   constructor(private firebaseDataService: FirebaseDataService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+
+    this.firebaseDataService.getData('menu').subscribe({
+      next: (items: any[]) => {
+        this.menuItems = this.groupByCategory(items);
+        this.loading = false;
+      },
+      error: err => {
+        console.error('Error al cargar el menú:', err);
+        this.loading = false;
+      }
+    });
+  }
 
   private groupByCategory(items: any[]): { [category: string]: any[] } {
     return items.reduce((acc: any, item: any) => {
